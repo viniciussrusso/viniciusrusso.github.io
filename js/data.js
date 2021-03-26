c19de29ecd128d77326533fcc746a6f7bf12a82e
Vinicius Russo <56481183+viniciussrusso@users.noreply.github.com>
2021-03-26 14:09:29 +0000

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,25 @@
+var data = {
+  name : 'Vinicius Russo',
+  bio: [
+    {name: 'Name', value: 'Vinicius Russo'},
+    {name: 'Email', value: 'viniciussrusso@outlook.com'},
+    {name: 'Phone', value: '+353 89 615 8299'},
+    {name: 'Country', value: 'Ireland'},
+    {name: 'City', value: 'Dublin'}
+  ],
+  github: 'viniciussrusso',
+  projects: [
+    {name: 'Covid Vaccination', url: 'https://github.com/viniciussrusso/CovidVacinnation'},
+    {name: 'BookStore', url: 'https://github.com/viniciussrusso/BookStore'},
+    {name: 'FileHandling-GUI', url: 'https://github.com/viniciussrusso/FileHandling-GUI'},
+    {name: 'Stay Safe', url: 'https://github.com/viniciussrusso/StaySafe19'},
+    {name: 'Countdown Game', url: 'https://github.com/viniciussrusso/KeywordCountdown'},
+    {name: 'MP Webpage', url: 'https://github.com/viniciussrusso/MP-webpage'}
+  ],
+  resume: 'viniciusrusso_cv.pdf',
+  socials: [
+    {name: 'LinkedIn', url: 'https://www.linkedin.com/in/vinicius-russo/'},
+    {name: 'GitHub', url: 'https://github.com/viniciussrusso'},
+    {name: 'Whatsapp', url: 'https://wa.me/353896158299'}
+  ]
+}
